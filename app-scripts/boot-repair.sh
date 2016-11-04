befc66d971a340cd6b94f26b7c1c84fa5cf894d9
cesar-rgon <cesar.rgon@gmail.com>
2016-11-04 15:16:28 +0000

    Added boot-repair utility

---
+++ app-scripts/boot-repair.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##########################################################################
+# This script starts the installation and configuration process of the
+# specified application and finally it shows a log file which contains
+# reported installation steps and posible errors.
+# @author César Rodríguez González
+# @version 1.3, 2016-11-04
+# @license MIT
+##########################################################################
+
+# Basic Variables
+scriptRootFolder="`pwd`/.."; username="`whoami`"; homeFolder="$HOME"
+
+# Import common variables and functions
+. ../common/commonVariables.properties
+. ../common/commonFunctions.sh
+declare -a appsToInstall=( "Boot_repair" )
+
+# Lauch menu and install selected applications
+prepareScript "$0"
+installAndSetupApplications appsToInstall[@]
