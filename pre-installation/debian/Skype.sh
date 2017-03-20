492958f874aab1eb62e68e741b10756e96398436
César Rodríguez González <cesar@debian-vbox>
2017-03-20 15:12:43 +0000

    Skype installation fix on Debian 9

--- pre-installation/debian/Skype.sh
+++ pre-installation/debian/Skype.sh
@@ -2,7 +2,7 @@
 ##########################################################################
 # This script prepares Skype application to be installed
 # @author César Rodríguez González
-# @version 1.3.3, 2017-03-19
+# @version 1.3.3, 2017-03-20
 # @license MIT
 ##########################################################################
 
@@ -18,5 +18,5 @@ if [ -n "$3" ]; then homeFolder="$3"; else homeFolder="$HOME"; fi
 . $scriptRootFolder/common/commonVariables.properties
 
 # Download and install needed dependency
-wget -O libssl1.0.0_i386.deb http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u6_i386.deb
-gdebi --n libssl1.0.0_i386.deb
+wget -O /var/cache/apt/archives/libssl1.0.0_i386.deb http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u6_i386.deb
+gdebi --n /var/cache/apt/archives/libssl1.0.0_i386.deb
