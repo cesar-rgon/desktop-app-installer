36dbc8b47a03677e743c33da4caeb3482cde067c
cesar-rgon <cesar.rgon@gmail.com>
2017-04-19 22:50:14 +0100

    Added VMWare Player application

---
+++ app-scripts/vmware-player.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+##########################################################################
+# This script starts the installation and configuration process of the
+# specified application and finally it shows a log file which contains
+# reported installation steps and posible errors.
+# @author César Rodríguez González
+# @version 1.3, 2016-08-12
+# @license MIT
+##########################################################################
+
+# Basic Variables
+scriptRootFolder="`pwd`/.."; username="`whoami`"; homeFolder="$HOME"
+
+# Import common variables and functions
+. $scriptRootFolder/common/commonFunctions.sh
+declare -a appsToInstall=( "VMWare_player" )
+
+# Lauch menu and install selected applications
+prepareScript "$0"
+installAndSetupApplications appsToInstall[@]
---
+++ install-non-repo-apps/VMWare_player_x64.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##########################################################################
+# This script installs VMWare Player application from external sources
+# @author César Rodríguez González
+# @version 1.3.3, 2017-04-19
+# @license MIT
+##########################################################################
+
+# Check if the script is being running by a root or sudoer user
+if [ "$(id -u)" != "0" ]; then echo ""; echo "This script must be executed by a root or sudoer user"; echo ""; exit 1; fi
+
+# Parameters
+if [ -n "$1" ]; then scriptRootFolder="$1"; else scriptRootFolder="`pwd`/../.."; fi
+if [ -n "$2" ]; then username="$2"; else username="`whoami`"; fi
+if [ -n "$3" ]; then homeFolder="$3"; else homeFolder="$HOME"; fi
+
+# Add common variables
+. $scriptRootFolder/common/commonVariables.properties
+
+# Commands to download, extract and install a non-repository application ...
+wget -O $tempFolder/vmware-player-x86_64.bundle https://download3.vmware.com/software/player/file/VMware-Player-12.5.5-5234757.x86_64.bundle 2>&1
+bash $tempFolder/vmware-player-x86_64.bundle
