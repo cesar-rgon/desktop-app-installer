fb15f68ae5abb074af6cd52773534c5a03c8d828
cesar-rgon <cesar.rgon@gmail.com>
2017-04-22 21:13:52 +0100

    Added Discord application

---
+++ install-non-repo-apps/Discord_i386.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+##########################################################################
+# This script sends a warning message. It's not possible to install
+# Discord application on 32 bits operating systems.
+# @author César Rodríguez González
+# @version 1.3.3, 2017-04-22
+# @license MIT
+##########################################################################
+
+# Check if the script is being running by a root or sudoer user
+if [ "$(id -u)" != "0" ]; then echo ""; echo "This script must be executed by a root or sudoer user"; echo ""; exit 1; fi
+
+# Parameters
+if [ -n "$1" ]; then scriptRootFolder="$1"; else scriptRootFolder="`pwd`/.."; fi
+if [ -n "$2" ]; then username="$2"; else username="`whoami`"; fi
+if [ -n "$3" ]; then homeFolder="$3"; else homeFolder="$HOME"; fi
+
+# Add common variables
+. $scriptRootFolder/common/commonVariables.properties
+
+if [ "$language" == "es" ]; then
+	message="Discord no está soportado para sistemas linux 32 bits. La aplicación no puede ser instalada"
+else
+	message="Discord application is not supported for 32 bits linux systems. It can not be installed"
+fi
+
+echo "$message" 1>&2
+if [ -n "$DISPLAY" ]; then
+	notify-send -i "$installerIconFolder/stop-error.png" "ERROR" "$message"
+  sleep 5
+fi
