d90b04f8d5930d2c87a705de6918aadfb530da14
César Rodríguez González <cesar@debian-vbox>
2017-04-22 15:49:48 +0100

    Added pre-installation script to Peazip application

---
+++ pre-installation/Peazip_x64.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##########################################################################
+# This script prepares Peazip application to be ready to be installed on
+# 64 bits OS linux
+# @author César Rodríguez González
+# @version 1.3.3, 2017-04-22
+# @license MIT
+##########################################################################
+
+# Check if the script is being running by a root or sudoer user
+if [ "$(id -u)" != "0" ]; then echo ""; echo "This script must be executed by a root or sudoer user"; echo ""; exit 1; fi
+
+# Parameters
+if [ -n "$1" ]; then scriptRootFolder="$1"; else scriptRootFolder="`pwd`/.."; fi
+if [ -n "$2" ]; then username="$2"; else username="`whoami`"; fi
+if [ -n "$3" ]; then homeFolder="$3"; else homeFolder="$HOME"; fi
+
+# Add common variables
+. $scriptRootFolder/common/commonVariables.properties
+
+# We must add 32 bits architecture to be able to install Peazip 32 bits in 64 bits OS
+dpkg --add-architecture i386
